43e5c50e30388003a76d6416ceadeb7c76ac2336
Thomas Nadin-Hepburn <tom.nadin.hepburn@gmail.com>
2024-04-23 14:39:16 +0100

    starting recorder history

--- screens/SwingRecorder.js
+++ screens/SwingRecorder.js
@@ -9,88 +9,88 @@ import VideoPlayer from 'react-native-native-video-player';
 const RECS_DIR = FileSystem.documentDirectory + "GA_RECS";
 
 export default function SwingRecorder() {
-  const cameraRef = useRef(null);
-  const isFocused = useIsFocused();
-  const [hasAudioPermission, setHasAudioPermission] = useState(null);
-  const [hasCameraPermission, setHasCameraPermission] =useState(null);
-  //const [camera, setCamera] = useState(null);
-  // const [type, setType] = useState(Camera.Constants.Type.back);
-  const [isRecording, setIsRecording] = useState(false);
-  const [recordedVideoURI, setRecordedVideoURI] = useState(null);
+  // const cameraRef = useRef(null);
+  // const isFocused = useIsFocused();
+  // const [hasAudioPermission, setHasAudioPermission] = useState(null);
+  // const [hasCameraPermission, setHasCameraPermission] =useState(null);
+  // //const [camera, setCamera] = useState(null);
+  // // const [type, setType] = useState(Camera.Constants.Type.back);
+  // const [isRecording, setIsRecording] = useState(false);
+  // const [recordedVideoURI, setRecordedVideoURI] = useState(null);
 
-  const onRecordingStart = () => {
-    setIsRecording(true);
-  };
+  // const onRecordingStart = () => {
+  //   setIsRecording(true);
+  // };
 
-  const onRecordingEnd = () => {
-    setIsRecording(false);
-  };
+  // const onRecordingEnd = () => {
+  //   setIsRecording(false);
+  // };
 
-  const toggleRecording = async () => {
-    if (isRecording) {
-      try {
-        await cameraRef.current.stopRecording();
+  // const toggleRecording = async () => {
+  //   if (isRecording) {
+  //     try {
+  //       await cameraRef.current.stopRecording();
 
-      } catch (error) {
-        console.error('Failed to stop recording', error);
-      }
-    } else {
-      try {
-        const data = await cameraRef.current.recordAsync()
-          .then(onRecordingStart)
-          .catch(error => console.error('Failed to start recording', error));
-        setRecordedVideoURI(data.uri)
-      } catch (error) {
-        console.error('Failed to start recording', error);
-      }
-    }
-  };
+  //     } catch (error) {
+  //       console.error('Failed to stop recording', error);
+  //     }
+  //   } else {
+  //     try {
+  //       const data = await cameraRef.current.recordAsync()
+  //         .then(onRecordingStart)
+  //         .catch(error => console.error('Failed to start recording', error));
+  //       setRecordedVideoURI(data.uri)
+  //     } catch (error) {
+  //       console.error('Failed to start recording', error);
+  //     }
+  //   }
+  // };
 
-  useEffect(() => {
-    (async () => {
-      const cameraStatus = await Camera.requestCameraPermissionsAsync();
-      setHasCameraPermission(cameraStatus.status === 'granted');
-      const audioStatus = await Camera.requestMicrophonePermissionsAsync();
-      setHasAudioPermission(audioStatus.status === 'granted');})();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const cameraStatus = await Camera.requestCameraPermissionsAsync();
+  //     setHasCameraPermission(cameraStatus.status === 'granted');
+  //     const audioStatus = await Camera.requestMicrophonePermissionsAsync();
+  //     setHasAudioPermission(audioStatus.status === 'granted');})();
+  // }, []);
 
-  if (hasCameraPermission === null || hasAudioPermission === null ) {
-    return <View />;
-  }
-  if (hasCameraPermission === false || hasAudioPermission === false) {
-      return <Text>No access to camera</Text>;
-    }
-  if (isFocused) {
-    return (
-    <View style={{flex : 1}}>
-      <Camera 
-        ratio="16:9" 
-        style={{flex : 1}} 
-        ref={cameraRef} 
-        type={Camera.Constants.Type.back}
-      />
+  // if (hasCameraPermission === null || hasAudioPermission === null ) {
+  //   return <View />;
+  // }
+  // if (hasCameraPermission === false || hasAudioPermission === false) {
+  //     return <Text>No access to camera</Text>;
+  //   }
+  // if (isFocused) {
+  //   return (
+  //   <View style={{flex : 1}}>
+  //     <Camera 
+  //       ratio="16:9" 
+  //       style={{flex : 1}} 
+  //       ref={cameraRef} 
+  //       type={Camera.Constants.Type.back}
+  //     />
 
-      {/* <Button
-        title="Flip"
-        onPress={() => {
-        setType(
-          type === Camera.Constants.Type.back
-          ? Camera.Constants.Type.front
-          : Camera.Constants.Type.back
-          );
-          }}>
-      </Button> */}
+  //     {/* <Button
+  //       title="Flip"
+  //       onPress={() => {
+  //       setType(
+  //         type === Camera.Constants.Type.back
+  //         ? Camera.Constants.Type.front
+  //         : Camera.Constants.Type.back
+  //         );
+  //         }}>
+  //     </Button> */}
 
-      <Button
-        title={isRecording ? 'Stop Video' : 'Take Video'}
-        onPress={toggleRecording}
-      />
-    </View>
-    );  
-  }
-  else {
+  //     <Button
+  //       title={isRecording ? 'Stop Video' : 'Take Video'}
+  //       onPress={toggleRecording}
+  //     />
+  //   </View>
+  //   );  
+  // }
+  // else {
     return <View/>
-  }
+  // }
 }
 
 const styles = StyleSheet.create({
---
+++ screens/SwingRecorderHistory.js
@@ -0,0 +1,11 @@
+import { StyleSheet ,Text, View, Button,} from 'react-native';
+
+
+export default function SwingRecorderHistory({ route, navigation }) {
+return (
+    <View>
+        <Text>MY SWINGS</Text>
+    </View>
+);
+
+}
